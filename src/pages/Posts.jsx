dc9ae8e42b5efb826a74098404172dfeb83e5124
Reece Peardon <peardon.reece@gmail.com>
2024-11-05 23:37:15 -0800

    continued setting up some routes for home and user post pages

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function Posts() {
+  const {id} = useParams()
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      console.log(data)
+    }
+    fetchPosts()
+  }, [])
   return (
     <div>
-      Posts
+      {id}
     </div>
   );
 }
